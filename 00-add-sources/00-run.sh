bbfab8bd65e0c342f1dd24a357a64430e63cff35
free-x <oroitburd@gmail.com>
2022-05-13 16:54:46 +0200

    Prepare Release change

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -e
 
+
 install -m 644 files/graph.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-on_chroot apt-key add - < files/grafana.gpg.key
-on_chroot apt-key add - < files/influx.gpg.key
-on_chroot << EOF
+if [ ${RELEASE} == "buster" ]
+then
+  on_chroot apt-key add - < files/grafana.gpg.key
+  on_chroot apt-key add - < files/influx.gpg.key
+else
+  install -m 644 files/influx.gpg "${ROOTFS_DIR}/usr/share/keyrings"
+  install -m 644 files/graphana.gpg "${ROOTFS_DIR}/usr/share/keyrings"  
+fi
+
+sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/graph.list"
+
 apt-get update
 EOF
